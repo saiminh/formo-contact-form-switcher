16133d0a392ba3a2fb6c9a5aec147657c7b9af30
saiminh <saiminh@gmail.com>
2024-06-01 14:21:14 +0100

    overflow editor issues

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'c9dd7ed89b68de89be13');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'fead62738fc0258f8f41');
